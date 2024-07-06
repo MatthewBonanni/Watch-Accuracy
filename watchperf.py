d50b3f14d4e9704ba5d6064a925af72300442d8a
Matthew Bonanni <mbonanni001@gmail.com>
2024-07-05 23:02:35 -0700

    Log bug fix

--- watchperf.py
+++ watchperf.py
@@ -51,7 +51,7 @@ def get_appDir():
         appDir = os.path.dirname(os.path.abspath(__file__))
     except:
         appDir = os.getcwd()
-    
+
     return appDir
 
 # Loads the application data if the corresponding file exists. Creates it if not.
@@ -81,7 +81,7 @@ def save_file(watch, filePath):
 def input_yesno(prompt, default):
     yes = set(['yes', 'y'])
     no = set(['no', 'n'])
-    
+
     if default:
         ynStr = "(Y/n)"
     else:
@@ -107,13 +107,13 @@ def input_int(prompt, options, cancel = False):
             i += 1
         if cancel:
             print("({0}) CANCEL".format(numOptions + 1))
-        
+
         try:
             choice = int(input("Select: "))
         except(ValueError, NameError):
             print("\nError: Invalid input\n")
             continue
-        
+
         if cancel:
             if choice < 1 or choice > numOptions + 1:
                 print("\nError: Invalid selection\n")
@@ -145,7 +145,7 @@ def measure_offset():
             break
         except:
             input("Failed to connect to NTP server.\nPress Enter to try again when the seconds hand reaches 12...")
-    
+
     # Estimate the measured time by rounding to the closest minute
     nearest_min = dt.timedelta(minutes = round(realTime.second/60))
     measuredTime = realTime.replace(second = 0, microsecond = 0) + nearest_min
@@ -164,10 +164,10 @@ def measure_offset():
                 print("\nError: Invalid input\n")
         else:
             break
-    
+
     # Write data to memory
     offset = (measuredTime - realTime).total_seconds()
-    
+
     measurement = {}
     measurement['utc'] = [realTime.isoformat()]
     measurement['measured'] = [measuredTime.isoformat()]
@@ -183,7 +183,7 @@ def measure_offset():
 appDir = get_appDir()
 appData = load_appData(appDir)
 
-print("\
+print(r"\
  _       __      __       __    ____            ____\n\
 | |     / /___ _/ /______/ /_  / __ \___  _____/ __/\n\
 | | /| / / __ `/ __/ ___/ __ \/ /_/ / _ \/ ___/ /_  \n\
@@ -216,7 +216,7 @@ else:
                                                 initialfile = appData['prevFile'], \
                                                 title = "Open file", \
                                                 filetypes = (("watch files","*.wat"),("all files","*.*")))
-            
+
             # Open file if possible
             try:
                 watch = json.loads(open(filePath).read())
@@ -224,7 +224,7 @@ else:
                 break
             except:
                 print("\nFile could not be read.\n")
-        
+
         # Create new watch file
         elif fileOpt == 2:
 
@@ -234,7 +234,7 @@ else:
             for key in watchKeys:
                 if key != 'data':
                     watch[key] = input("{0}: ".format(key))
-        
+
             watch['data'] = ""
 
             # Select file via GUI
@@ -246,7 +246,7 @@ else:
                                                     title = "Save file", \
                                                     defaultextension = ".wat", \
                                                     filetypes = (("watch files","*.wat"),("all files","*.*")))
-            
+
             # Save newly made file if possible
             try:
                 save_file(watch, filePath)
@@ -254,7 +254,7 @@ else:
                 break
             except:
                 print("\nNo file saved.\n")
-        
+
         # Quit application
         elif fileOpt == 3:
             quit_app(appDir)
@@ -270,7 +270,7 @@ while True:
                                             "Save watch file", \
                                             "Save and exit", \
                                             "Exit without saving"])
-    
+
     # Take new measurement
     if actionOpt == 1:
 
@@ -280,7 +280,7 @@ while True:
             newSeries = input_yesno("Have you adjusted your watch time since the last measurement?", False)
         else:
             newSeries = True
-        
+
         # Take measurement
         dataPt = measure_offset()
 
@@ -299,7 +299,7 @@ while True:
             watch['data'][-1]['t_elapsed_total'] = [0]
             watch['data'][-1]['t_human'] = [str(dt.timedelta(seconds = 0))]
             watch['data'][-1]['rate'] = [None]
-        
+
         # Add measurement to current series
         else:
 
@@ -315,7 +315,7 @@ while True:
             dataPt_calc['t_elapsed_split'] = (new_datetime - prev_datetime).total_seconds()
             dataPt_calc['t_elapsed_total'] = (new_datetime - first_datetime).total_seconds()
             dataPt_calc['t_human'] = str(new_datetime - first_datetime)
-            
+
             dataPt_calc['rate'] = dataPt_calc['offset_change'] / (dataPt_calc['t_elapsed_split'] / 86400)
 
             # Append measurement to last series
@@ -324,7 +324,7 @@ while True:
                     watch['data'][-1][key].append(dataPt[key][0])
                 else:
                     watch['data'][-1][key].append(dataPt_calc[key])
-    
+
     # View past measurement data
     elif actionOpt == 2:
         if watch['data'] == "":
@@ -338,16 +338,16 @@ while True:
                     for key in dataKeys:
                         row.append(series[key][i])
                     seriesTable.append(row)
-                
+
                 print("")
                 print(tabulate.tabulate(seriesTable, headers = 'firstrow', tablefmt = 'simple'))
-    
+
     # Delete the last measurement
     elif actionOpt == 3:
         if watch['data'] == "":
             print("\nNo data exists to delete.")
             continue
-        
+
         confirmDeleteOpt = input_yesno("\nAre you sure you want to delete the last data point?", False)
         if confirmDeleteOpt:
             if len(watch['data']) == 1 and len(watch['data'][0]['measured']) == 1:
@@ -360,7 +360,7 @@ while True:
         else:
             print("Delete operation cancelled.")
             continue
-    
+
     # Analyze the watch performance based on data in memory
     elif actionOpt == 4:
 
@@ -368,17 +368,17 @@ while True:
         if watch['data'] == "":
             print("\nA minimum of 1 series with 2 data points is required for analysis.")
             continue
-        
+
         # Verify at least 2 data points
         seriesWith2 = 0
         for series in watch['data']:
             if len(series['rate']) >= 2:
                 seriesWith2 += 1
-        
+
         if seriesWith2 == 0:
             print("\nA minimum of 1 series with 2 data points is required for analysis.")
             continue
-        
+
         # Compute total elapsed time across all series
         totalTime = 0
         elapsedTimeWarning = 0
@@ -388,7 +388,7 @@ while True:
                 if (series['t_elapsed_total'][-1] / 3600) < 12:
                     elapsedTimeWarning = 1
                 totalTime += series['t_elapsed_total'][-1]
-        
+
         # Compute weighted average of offset rate
         # Each measurement is weighted by its split duration
         meanRate = 0
@@ -402,9 +402,9 @@ while True:
 
         if elapsedTimeWarning:
             print("\nWarning: For accurate performance analysis, series data should span at least 12 hours.")
-        
+
         print("\nTime-weighted mean deviation rate is {0} seconds per day.".format(meanRate))
-    
+
     # View or edit the watch metadata
     elif actionOpt == 5:
         print("")
@@ -414,7 +414,7 @@ while True:
         for key in watchKeys:
             if key != 'data':
                 currentInfo.append("{0}: {1}".format(key, watch[key]))
-        
+
         # Edit data menu
         infoOpt = input_int("Change which item?", currentInfo, cancel = True)
 
@@ -423,18 +423,18 @@ while True:
             continue
         else:
             watch[watchKeys[infoOpt-1]] = input("Enter new value for {0}: ".format(watchKeys[infoOpt-1]))
-    
+
     # Save without quitting
     elif actionOpt == 6:
         save_file(watch, filePath)
         print("\nSaved to {0}".format(filePath))
-    
+
     # Save and quit
     elif actionOpt == 7:
         save_file(watch, filePath)
         print("\nSaved to {0}".format(filePath))
         quit_app(appDir)
-    
+
     # Quit without saving
     elif actionOpt == 8:
         confirmExitOpt = input_yesno("\nAre you sure you want to exit without saving?", False)
